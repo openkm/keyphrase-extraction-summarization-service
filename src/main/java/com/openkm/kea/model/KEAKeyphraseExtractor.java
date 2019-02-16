9319f1a4ff34ec12a115b27083d9796a5c5db129
jllort <jllort@openkm.com>
2019-02-16 20:54:54 +0100

    removing eclise files

--- src/main/java/com/openkm/kea/model/KEAKeyphraseExtractor.java
+++ src/main/java/com/openkm/kea/model/KEAKeyphraseExtractor.java
@@ -1,5 +1,26 @@
 package com.openkm.kea.model;
 
+/*-
+ * #%L
+ * kea-summarization
+ * %%
+ * Copyright (C) 2018 - 2019 OpenKM Knowledge Management System S.L.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 /*
  *    KEAKeyphraseExtractor.java
  *    Copyright (C) 2001-2006 Eibe Frank, Olena Medelyan
